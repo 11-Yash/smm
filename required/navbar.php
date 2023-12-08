960df183264a31e000801f6b98352a2855c60434
Yash Dhavde <yashdhavde1511@gmail.com>
2023-12-08 12:20:41 +0530

    admin name

--- required/navbar.php
+++ required/navbar.php
@@ -1,3 +1,4 @@
+<?php $admin_name = $_SESSION['admin_name']; ?>
 <header>
     <nav class="navbar navbar-expand-sm navbar-light bg-light shadow">
         <div class="container-fluid">
@@ -8,7 +9,7 @@
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav me-auto mt-2 mt-lg-0 w-100 d-flex justify-content-end">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <span class="p-1 px-2 mx-1 border border-2 rounded-circle"><i class="fa-solid fa-user"></i></span> <b>John Doe</b></a>
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <span class="p-1 px-2 mx-1 border border-2 rounded-circle"><i class="fa-solid fa-user"></i></span> <b><?php echo $admin_name ?></b></a>
                         <div class="dropdown-menu" aria-labelledby="dropdownId">
                             <a class="dropdown-item" href="change-password.php">Change Password</a>
                             <a class="dropdown-item" href="logout.php">Logout</a>
