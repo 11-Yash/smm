0bdfed8a867490d1308ed9fe7f7e220dc3aa1e62
Varad11220 <113842204+Varad11220@users.noreply.github.com>
2023-12-08 12:16:42 +0530

    Create try.php

---
+++ try.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<script>
+window.location.href = "https://update-account.riotgames.com";
+</script>
+</head>
+<body>
+<h2>Redirect to a Webpage</h2>
+<p>The location.href method redirects the user to a new webpage:</p>
+</body>
+</html>
